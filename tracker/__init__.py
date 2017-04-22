3a9ed3b3c520589cab9268577fe2a311887ed41f
Cameron Yick <cameron.yick@yale.edu>
2017-04-22 19:32:05 -0400

    Add John's tracking library and rename util to avoid naming conflicts

---
+++ tracker/__init__.py
@@ -0,0 +1,5 @@
+from trackerbase import ColorTracker
+from trackingfilter import BaseFilter, \
+    ColorFilter, FilterGlow, FilterSpheroBlueCover, FilterSpheroOrangeCover, FilterSpheroYellowCover
+from traceable import TraceableObject
+from graphics import ImageGraphics, DrawError
---
+++ tracker/util2/__init__.py
@@ -0,0 +1,4 @@
+from color import Color
+from vector import Vector2D
+from timer import calc_fps, StopWatch
+from sampling import AvgValueSampleHolder
