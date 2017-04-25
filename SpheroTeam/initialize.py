218a875da0e9e3b18c9494a24c776a78ad1ebf9e
Cameron Yick <cameron.yick@yale.edu>
2017-04-24 22:30:22 -0400

    Create Initialization, Update Util Modules
    Continued refactoring of Sphero Control code from the exploratory notebooks.

---
+++ SpheroTeam/initialize.py
@@ -0,0 +1,63 @@
+# initialize.py
+# These will probably be called any time you want to control multiple spheros
+# Methods that utilize a SpheroManager
+# REFACTOR: Make these a class method under the SpheroTeam object
+
+from util import readJsonFile
+from core import connect_team
+import logging
+
+
+def load_sphero_roster(manager, configFile):
+    '''Initialize Sphero Objects from a configuration file specifying their
+        names and MAC addresses.
+
+        :param manager: Team Manager
+        :param configFile: Path to configuration file, such as config.json
+
+        :type SpheroManager: SpheroManager created via sphero.SpheroManager()
+        :type configFile: String
+
+        Returns modified manager object.
+    '''
+    config = readJsonFile(configFile)
+    roster = readJsonFile(config['teamFile'])
+    manager._name_cache = roster
+
+    for bt_addr, bt_name in roster.iteritems():
+        manager.add_sphero(bt_addr, bt_name)
+
+    return manager
+
+
+def connect_sphero_team(manager, botAbbreviations=None):
+    '''
+        Return list of connected sphero objects.
+        By default, attempts to connect to every single robot in config file
+
+        (.e.g RWR, ORG, RYR, etc)
+
+        :param manager: Team Manager
+        :param botAbbreviations: List of 3-letter capital Sphero Namestrings
+
+        :type SpheroManager: SpheroManager created via sphero.SpheroManager()
+        :type configFile: String
+    '''
+
+    # Assumes that every robot requested was already added in the config file.
+    bots = []
+
+    if botAbbreviations:
+        for abbr in botAbbreviations:
+            bot = manager._spheros.get("Sphero-{}".format(abbr))
+            if bot:
+                bots.append(bot)
+            else:  # Consider whether to raise a python Exception too
+                logging.warning("Sphero-%s not found", abbr)
+
+    else:
+        for bt_name, bot in manager._spheros.iteritems():
+            bots.append(bot)
+
+    connect_team(bots)
+    return bots
