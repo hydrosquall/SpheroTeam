7aa3f8067a6ae4b51e76865d26f6cd7d0d05d283
Cameron Yick <cameron.yick@yale.edu>
2017-04-26 00:20:31 -0400

    PEP8 Validation of Directory
    Cleanup and document director file

--- SpheroTeam/director.py
+++ SpheroTeam/director.py
@@ -1,5 +1,5 @@
 # Director.py
-# Prototyped in notebooks 7, 8
+# Prototyped in notebooks 7, 8, 9
 
 import time
 import math
@@ -10,19 +10,26 @@ from core import roll_sphero
 
 
 def bot_go_to_point(bot, offset,
-                     targetX, targetY,
-                     trace_object, trace_color,
-                     MAX_X, MAX_Y, tracker,
-                     TIMEOUT=250, MAX_SECONDS=10, 
-                     stopRadius=30, Kp=0.23, DEBUG=False):
-    """
-    Constrast to "RUSH TO POINT IN STRAIGHT LINE"
+                    targetX, targetY,
+                    trace_object, trace_color,
+                    MAX_X, MAX_Y, tracker,
+                    TIMEOUT=250, MAX_SECONDS=10,
+                    stopRadius=30, Kp=0.25, DEBUG=False):
+    """ Sends robot to a desired point
 
-    Really would work better with a bigger field
+    :param bot: robot being controlled
+    :param targetX: x coordinate
+    :param targetY: y coordinate
+    :param MAX_X: maximum x dimension
+    :param TIMEOUT: How many milliseconds pass before issuing new command
+    :param MAX_SECONDS: How many seconds to navigate before quitting
+    :param stopRadius: Pixel distance threshhold before stopping
+    :param Kp: Proportional control constant
+    :param DEBUG: Boolean toggling print statements
 
-    # TIMEOUT should be longer than  get_position's runtime
+    :type trace_object: traceable object
+    :type trace_color: RGB list of values
 
-    Or boost speed when within range: pause and then burst
     # With thanks to Rabia Aslam for ideas about the PID controller
     http://rabiaaslam.com/designing-the-sphero-control-system/
     """
@@ -30,52 +37,60 @@ def bot_go_to_point(bot, offset,
     if (targetX > MAX_X) or (targetY > MAX_Y):
         logging.warning("Can't roll off the screen! %d %d", MAX_X, MAX_Y)
         return -1
-    sleepTime = TIMEOUT/1000.0
+    sleepTime = TIMEOUT / 1000.0
 
-    currentX, currentY = get_bot_position(bot, trace_object, tracker, samples=1)
+    currentX, currentY = get_bot_position(bot, trace_object,
+                                          tracker, samples=2)
 
     # Basic closed loop controller
     startTime = time.time()
 
     # Angle to distance
     angle, distance = vector_to_target(currentX, currentY, targetX, targetY)
-    print("Get from {},{} to {},{}| Distance {} / {}").format(currentX, currentY, 
-                                                         targetX, targetY, distance, angle)
+    print("Go {},{} to {},{}| Distance {} / {}").format(currentX, currentY,
+                                                        targetX, targetY,
+                                                        distance, angle)
     bot.set_motion_timeout(TIMEOUT)
     # REPLACE SOMEDAY WITH TRUE PID CONTROLLER
-    while distance > stopRadius and (((time.time() - startTime ) < MAX_SECONDS)):
-        outSpeed = distance * Kp # Constant of proportional control
+    while (distance > stopRadius) \
+            and \
+            (((time.time() - startTime) < MAX_SECONDS)):
+
+        outSpeed = distance * Kp  # Constant of proportional control
 
         if outSpeed < 30:
             outSpeed = 35
 
         # roll the sphero, make use of the request object
         if DEBUG:
-            print("Dist {} outSpeed {} at {} degrees: {},{}"\
+            print("Dist {} outSpeed {} at {} degrees: {},{}"
                   .format(distance, outSpeed, angle, currentX, currentY))
 
         roll_sphero(bot, outSpeed, -angle, offset)
 
-        # use this for recovery when lost
-
-        bot.prev_angle = -angle   
+        # use this for recovery when at boundary
+        bot.prev_angle = -angle
         time.sleep(sleepTime)
 
-        currentX , currentY = get_bot_position(bot, trace_object, tracker, samples=1)
+        currentX, currentY = get_bot_position(bot, trace_object,
+                                              tracker, samples=2)
 
         if currentX:
             # Repeat waypointing calculation
-            angle, distance = vector_to_target(currentX, currentY, targetX, targetY)
+            angle, distance = vector_to_target(currentX, currentY,
+                                               targetX, targetY)
         else:
             # bring robot back onto screen
             angle = bot.prev_angle
-            distance = 100 # aim to go back to middle
-            
-    print("Stopped at {},{}, with dist {}").format(currentX, currentY, distance)
+            distance = 100  # aim to go back to middle
+
+    print("Stopped at {},{}, with dist {}").format(currentX, currentY,
+                                                   distance)
+
 
 def vector_to_target(currentX, currentY, targetX, targetY):
     '''
-        Returns distance and angle between two points
+        Returns distance and angle between two points (in degrees)
     '''
     deltaX = targetX - currentX
     deltaY = targetY - currentY
@@ -84,55 +99,84 @@ def vector_to_target(currentX, currentY, targetX, targetY):
 
     return angle, distance
 
-# Team driving functions
-def team_go_to_points(bots, targets, offsets, traceable_object, traceable_color,
-                      MAX_X=imageX, MAX_Y=imageY, TIMEOUT=200, stopRadius=25):
-    '''
-        Send each bot in the team to its designated point. 
-        Bots drive in a straight line to
-        each point.
 
-        Alternate function would send each robot along paths (sequences of points)
+# Team driving functions
+def team_go_to_points(bots, targets, offsets,
+                      traceable_object, traceable_color,
+                      MAX_X, MAX_Y, tracker,
+                      TIMEOUT=200, stopRadius=25, Kp=0.23):
+    '''
+        Send each bot in the team to its designated point in targets
+
+        :param bot: robot being controlled
+        :param targets: A list of (x,y) coordinate pairs
+        :param MAX_X: maximum x dimension
+        :param MAX_Y: maximum Y dimension
+        :param tracker: ColorTracker() object
+        :param TIMEOUT: How many milliseconds pass before issuing new command
+        :param stopRadius: Pixel distance threshhold before stopping
+        :param Kp: Proportional control constant
+
+        :type traceable_object: traceable object in SpheroNav library
+        :type traceable_color: RGB list of values
+
+        # Ideas for refactor
         Perhaps each point might have its own Kp or timeout or speed.
     '''
 
+    # Turn each robot's light off before starting
     for bot in bots:
-        bot.set_rgb(0,0,0)
+        bot.set_rgb(0, 0, 0)
 
     for i, bot in enumerate(bots):
         print("Bot {}".format(bot.bt_name))
         bot.set_rgb(traceable_color[0], traceable_color[1], traceable_color[2])
-        time.sleep(1.5)
+        time.sleep(1.5)  # Give camera time to adjust
 
         targetX, targetY = targets[i]
         bot_go_to_point(bot, offsets[i], targetX, targetY,
-                        traceable_object, traceable_color, MAX_X, MAX_Y, 
-                        tracker, TIMEOUT=200, stopRadius=stopRadius)
+                        traceable_object, traceable_color, MAX_X, MAX_Y,
+                        tracker, TIMEOUT=200, stopRadius=stopRadius, Kp=Kp)
 
-        bot.set_rgb(0,0,0)
-
+        bot.set_rgb(0, 0, 0)
 
 
 def team_go_to_paths(bots, paths, offsets, traceable_object, traceable_color,
-                      MAX_X, MAX_Y, TIMEOUT=200, stopRadius=25):
+                     MAX_X, MAX_Y, tracker, TIMEOUT=200,
+                     stopRadius=25, Kp=0.23):
     '''
         Send each bot in the team a list of points
+
+        :param bots: a list of Sphero objets (a SpheroTeam)
+        :param paths: A list of lists list of (x,y) coordinate pairs, 1 per bot
+        :param MAX_X: maximum x dimension
+        :param MAX_Y: maximum Y dimension
+        :param tracker: ColorTracker() object
+        :param TIMEOUT: How many milliseconds pass before issuing new command
+        :param MAX_SECONDS: How many seconds to navigate before quitting
+        :param stopRadius: Pixel distance threshhold before stopping
+        :param Kp: Proportional control constant
+        :param DEBUG: Boolean toggling print statements
+
+        :type traceable_object: traceable object in SpheroNav library
+        :type traceable_color: RGB list of values
     '''
-    
+
+    # Start by turning off each robot
     for bot in bots:
-        bot.set_rgb(0,0,0)
-    
+        bot.set_rgb(0, 0, 0)
+
     for i, bot in enumerate(bots):
         print("Bot {}".format(bot.bt_name))
         bot.set_rgb(traceable_color[0], traceable_color[1], traceable_color[2])
         time.sleep(1.5)
-        
 
         for target in paths[i]:
             targetX, targetY = target
-            bot_go_to_point(bot, offsets[i], targetX, targetY,          
-                traceable_object, traceable_color, MAX_X, MAX_Y, tracker, TIMEOUT=250,
-                           stopRadius=stopRadius)  
-        
+            bot_go_to_point(bot, offsets[i], targetX, targetY,
+                            traceable_object, traceable_color,
+                            MAX_X, MAX_Y, tracker, TIMEOUT=250,
+                            stopRadius=stopRadius, Kp=Kp)
+
         # Turn robot off when done
-        bot.set_rgb(0,0,0) 
+        bot.set_rgb(0, 0, 0)
