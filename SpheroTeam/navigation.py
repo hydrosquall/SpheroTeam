397cb50688fa3ad8ba46b582f1a9c67456dc1171
Cameron Yick <cameron.yick@yale.edu>
2017-04-25 21:26:37 -0400

    Speed up location tracking algorithm
    Speed up position tracker to cut down on tracking latency and improve feedback

--- SpheroTeam/navigation.py
+++ SpheroTeam/navigation.py
@@ -15,29 +15,35 @@ def get_bot_position(bot, traceable_object, tracker, samples=3, debug=False):
         Assumes that bot is visible, return x,y position of that robot
         Not sure how necessary samples are
     """
-    xSamples = []
-    ySamples = []
+    # xSamples = []
+    # ySamples = []
+
+    xSum = 0
+    ySum = 0
     nSamples = 0
 
-    for sample in range(samples):
+    for sample in xrange(samples):
         image = tracker.get_video_frame()
-        if sample > 0:  # ignore the first sample
-        
-            # side effect: adds mask to tracker  
-            x, y = tracker._find_traceable_in_image(image, traceable_object)
+        # if sample > 0:  # ignore the first sample
 
-            # As long as x and y are not none, store both samples.
-            if x:
-                xSamples.append(x)
-                ySamples.append(y)
-                nSamples += 1
+        # side effect: adds mask to tracker  
+        x, y = tracker._find_traceable_in_image(image, traceable_object)
+
+        # As long as x and y are not none, store both samples.
+        if x:
+            # xSamples.append(x)
+            # ySamples.append(y)
+            xSum += x
+            ySum += y
+            nSamples += 1
 
     # if debug:
     #     camera.display_current_view(tracker)
     #     print "{} | {} ".format(xSamples, ySamples)
 
     if nSamples > 0:
-        return (sum(xSamples) / nSamples), (sum(ySamples) / nSamples)
+        # return (sum(xSamples) / nSamples), (sum(ySamples) / nSamples)
+        return xSum / nSamples, ySum / nSamples
     else:
         logging.warning("Robot not in view, make sure it's on?")
         return None, None
@@ -51,23 +57,39 @@ def calibrate_bot_direction(bot, traceable_object, traceable_color,
 
     bot.set_rgb(traceable_color[0], traceable_color[1], traceable_color[2])
     bot.set_motion_timeout(TIMEOUT)
-    time.sleep(TIMEOUT/1000)  # Give robot time to change color and camera to see it
+    time.sleep(1)  # Give robot time to change
 
     startX, startY = get_bot_position(bot, traceable_object, tracker)
-    cv2.waitKey(100)  # not sure how long this wait has to be
+    cv2.waitKey(100)       # not sure how long this wait has to be
 
     if (startX is None) or (startY is None):
         print("Error: Robot not in view")
         return -1
 
-    print "Start ({},{})".format(startX, startY)
-
     bot.roll(60, 0)  # reconfigure later
     time.sleep(TIMEOUT / 1000)
     endX, endY = get_bot_position(bot, traceable_object, tracker)
 
-    print "End   ({},{})".format(endX, endY)
     offset = normalize_angle(angle_between_points(startX, startY, endX, endY))
-    print "Angle {}".format(offset)
+
+    if debug:
+        print "Start ({},{})".format(startX, startY)
+        print "End   ({},{})".format(endX, endY)
+        print "Angle {}".format(offset)
+
     bot.set_rgb(0, 0, 0)
     return offset
+
+def get_team_offsets(bots, traceable_object, traceable_color, tracker):
+    offsets = []
+
+    for bot in bots:
+        offset = calibrate_bot_direction(bot, traceable_object,
+                                         traceable_color, tracker, True)
+        offsets.append(offset)
+        proceed = raw_input("'q' to quit, else calibrate next robot ")
+        if proceed == "q":
+            break
+        else:
+            continue
+    return offsets
